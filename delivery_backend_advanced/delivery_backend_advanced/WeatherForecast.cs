16ea61918f2681c360e8cf22248aded84605223a
DanilGrasmik <dankagrasmik@gmail.com>
2023-03-25 20:16:53 +0700

    init commit

---
+++ delivery_backend_advanced/delivery_backend_advanced/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace delivery_backend_advanced;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
