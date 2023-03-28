e964911c2b4a0d0d771cab0d49391cac75fad691
DanilGrasmik <dankagrasmik@gmail.com>
2023-03-28 21:04:06 +0700

    Mapper

---
+++ delivery_backend_advanced/delivery_backend_advanced/Models/AppMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using delivery_backend_advanced.Models.Dtos;
+using delivery_backend_advanced.Models.Entities;
+
+namespace delivery_backend_advanced.Models;
+
+public class AppMappingProfile : Profile
+{
+    public AppMappingProfile()
+    {
+        CreateMap<RestaurantEntity, RestaurantListElementDto>();
+        CreateMap<MenuEntity, MenuDto>().ReverseMap();
+        CreateMap<DishEntity, DishListElementDto>();
+        CreateMap<RestaurantEntity, RestaurantDetailsDto>();
+    }
+}
