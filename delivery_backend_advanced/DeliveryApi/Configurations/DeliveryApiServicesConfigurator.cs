e213165246e35be54efc49a530ab80cfb6cf16cd
DanilGrasmik <dankagrasmik@gmail.com>
2023-04-12 21:20:47 +0700

    Configuration for services

---
+++ delivery_backend_advanced/DeliveryApi/Configurations/DeliveryApiServicesConfigurator.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using System.Text.Json.Serialization;
+using delivery_backend_advanced.Services;
+using delivery_backend_advanced.Services.Interfaces;
+using DeliveryApi.BL.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace delivery_backend_advanced.Models;
+
+public static class DeliveryApiBLConfigurator
+{
+    public static void ConfigureDeliveryApiServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddAutoMapper(typeof(AppMappingProfile));
+        
+        builder.Services.AddScoped<IRestaurantService, RestaurantService>();
+        builder.Services.AddScoped<IDishService, DishService>();
+        builder.Services.AddScoped<IBasketService, BasketService>();
+        builder.Services.AddScoped<IOrderService, OrderService>();
+        builder.Services.AddScoped<ICookService, CookService>();
+        builder.Services.AddScoped<ICourierService, CourierService>();
+        builder.Services.AddScoped<IManagerService, ManagerService>();
+        
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(options =>
+        {
+            var xmlFilename = $"{ Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+        });
+        
+        builder.Services
+            .AddControllers()
+            .AddJsonOptions(options => 
+                options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+    }
+}
--- delivery_backend_advanced/DeliveryApi/Program.cs
+++ delivery_backend_advanced/DeliveryApi/Program.cs
@@ -1,40 +1,10 @@
-using System.Reflection;
-using System.Text.Json.Serialization;
 using delivery_backend_advanced.Models;
-using delivery_backend_advanced.Services;
 using delivery_backend_advanced.Services.ExceptionHandler;
-using delivery_backend_advanced.Services.Interfaces;
-using DeliveryApi.BL.Services;
 using DeliveryApi.DAL;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    var xmlFilename = $"{ Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-});
-
-builder.Services.AddAutoMapper(typeof(AppMappingProfile));
-
-builder.Services.AddScoped<IRestaurantService, RestaurantService>();
-builder.Services.AddScoped<IDishService, DishService>();
-builder.Services.AddScoped<IBasketService, BasketService>();
-builder.Services.AddScoped<IOrderService, OrderService>();
-builder.Services.AddScoped<ICookService, CookService>();
-builder.Services.AddScoped<ICourierService, CourierService>();
-builder.Services.AddScoped<IManagerService, ManagerService>();
-
-builder.Services
-    .AddControllers()
-    .AddJsonOptions(options => 
-        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+builder.ConfigureDeliveryApiServices();
 
 builder.ConfigureDeliveryApiDAL();
 
