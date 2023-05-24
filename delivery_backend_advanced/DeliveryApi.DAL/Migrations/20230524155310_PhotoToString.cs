4781faccd5e4c40c4162c36abfa48507e569f3f1
DanilGrasmik <dankagrasmik@gmail.com>
2023-05-24 23:21:40 +0700

    backend migration

---
+++ delivery_backend_advanced/DeliveryApi.DAL/Migrations/20230524155310_PhotoToString.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DeliveryApi.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class PhotoToString : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Photo",
+                table: "Dishes",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "bytea");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "Photo",
+                table: "Dishes",
+                type: "bytea",
+                nullable: false,
+                defaultValue: new byte[0],
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
